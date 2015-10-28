3ec3852c50a92c098ed617292dac8195ab9b606a
ET <et@ETs-MacBook-Air.local>
2015-10-28 21:12:16 +1100

    Retrieved first character.

--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,34 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+url = "https://en.wikipedia.org/wiki/List_of_Star_Wars_characters"
+page = agent.get(url)
 #
 # # Find somehing on the page using css selectors
-# p page.at('div.content')
+
+page.search('dt').text
+page.search('dd').text
+page.at('dl').at('dt').text
+page.at('dl').at('dd').text
+
+character = {
+  character: page.at('dl').at('dt').text,
+  description: page.at('dl').at('dd').text
+}
+
+p character
+
 #
 # # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+
+#ScraperWiki.save_sqlite(["name"], {name: name, description: description})
+
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
